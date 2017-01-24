9ea6825c71702c1ddc55a448639e94bb86f28fa1
Heru Hermawan <heru@chocochipz.com>
2017-01-24 18:33:45 +0700

    Add main.php

--- footer.php
+++ footer.php
@@ -1,10 +1,5 @@
 	<div class="copyright">
 		<p>&copy; <?php echo date('Y');?> - Leaflet Test for Coolasia</p>
 	</div>	
-
-	<!-- BEGIN CORE TEMPLATE JS --> 
-
-	<!-- END PAGE LEVEL PLUGINS --> 	
-	
 </body>
 </html>
