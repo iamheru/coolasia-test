2bbc68c80be947ba5597f7ec26055fda35feef91
chocochipz <heru@chocochipz.com>
2017-01-24 17:54:55 +0700

    Update config.php
    Remove the original file in string content

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php 
-$dbhostname = "localhost"; 
-$dbusername = "root"; 
-$dbpassword = "";
-$dbdatabase = "coolasia"; 
+$dbhostname = "YOUR DB HOSTNAME"; 
+$dbusername = "YOUR DB USERNAME"; 
+$dbpassword = "YOUR DB PASSWORD";
+$dbdatabase = "YOUR DB"; 
 $conn = new mysqli($dbhostname,$dbusername,$dbpassword,$dbdatabase); 
 if (!$conn) {
-	die ("Error conecting to database server"); 
+	die ("Error connecting to database server"); 
 	exit;
 }
 ?>
